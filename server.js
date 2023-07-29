74a72d1dcdd1a2e292c79e8726a0587e6a20535d
dens <wewe@gmail.com>
2023-07-28 23:34:53 -0400

    server update

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB700 – Assignment 06
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Dennis Castro Student ID: 122875222 Date: July 28, 2023
+*
+*  Online (Cycliic) Link: https://nice-earrings-lion.cyclic.app
+*
+********************************************************************************/ 
+
 const express = require("express");
 const path = require("path");
 const collegeData = require("./modules/collegeData");
